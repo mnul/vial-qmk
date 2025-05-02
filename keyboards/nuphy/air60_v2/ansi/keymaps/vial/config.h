acb63d99aec13dd7b8cd4207fe749e7815a60813
mnul <junk@pixelemotionen.de>
2025-05-03 00:13:15 +0200

    update rules, keymap and configuration

---
+++ keyboards/nuphy/air60_v2/ansi/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4F, 0xD6, 0xE4, 0xC1, 0x59, 0x28, 0x82, 0xAC}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }  // Row for Esc, Row for Enter
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 } // Col for Esc, Col for Enter
