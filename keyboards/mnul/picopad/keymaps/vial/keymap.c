0dddb0865017f856527bf7ade10b2d72e2f9bcc0
mnul <junk@pixelemotionen.de>
2025-05-18 17:41:51 +0200

    first working firmware build, pins not in order

--- keyboards/mnul/picopad/keymaps/vial/keymap.c
+++ keyboards/mnul/picopad/keymaps/vial/keymap.c
@@ -1,28 +1,28 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT( /* Base Layer */
         KC_1, KC_2,
         KC_3, KC_4,
         KC_5, KC_6,
         KC_7, KC_8,
         KC_9, KC_0
     ),
-    [1] = LAYOUT(
+    [1] = LAYOUT( /* Layer 1 */
         KC_A, KC_B,
         KC_C, KC_D,
         KC_E, KC_F,
         KC_G, KC_H,
         KC_I, KC_J
     ),
-    [2] = LAYOUT(
+    [2] = LAYOUT( /* Layer 2 */
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS
     ),
-    [3] = LAYOUT(
+    [3] = LAYOUT( /* Layer 3 */
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
--- keyboards/mnul/picopad/mnul_picopad.c
+++
@@ -1,5 +0,0 @@
-#include QMK_KEYBOARD_H // This will include "mnul_picopad.h" and other QMK core headers
-
-// Keyboard-level C code can go here if needed.
-// For many simple keyboards, this file might be very minimal if
-// all keymap-specific logic is in the keymaps/default/keymap.c file.
