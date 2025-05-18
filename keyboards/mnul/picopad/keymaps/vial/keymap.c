6660f196b6063418186130a34cbaee6d5258c44c
mnul <junk@pixelemotionen.de>
2025-05-18 18:30:14 +0200

    increased dynamic layers to 8

--- keyboards/mnul/picopad/keymaps/vial/keymap.c
+++ keyboards/mnul/picopad/keymaps/vial/keymap.c
@@ -1,28 +1,56 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( /* Base Layer */
+    [0] = LAYOUT(
         KC_1, KC_2,
         KC_3, KC_4,
         KC_5, KC_6,
         KC_7, KC_8,
         KC_9, KC_0
     ),
-    [1] = LAYOUT( /* Layer 1 */
+    [1] = LAYOUT(
         KC_A, KC_B,
         KC_C, KC_D,
         KC_E, KC_F,
         KC_G, KC_H,
         KC_I, KC_J
     ),
-    [2] = LAYOUT( /* Layer 2 */
+    [2] = LAYOUT(
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS
     ),
-    [3] = LAYOUT( /* Layer 3 */
+    [3] = LAYOUT(
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS
+    ),
+    [4] = LAYOUT(
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS
+    ),
+    [5] = LAYOUT(
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS
+    ),
+    [6] = LAYOUT(
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS
+    ),
+    [7] = LAYOUT(
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
